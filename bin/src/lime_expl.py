b555fa90e1166df8def87e0cb8d4f180849eeed8
Stanisław Wasik <st.wasik@gmail.com>
2021-05-04 12:54:30 +0200

    Added LIME for regressors

---
+++ bin/src/lime_expl.py
@@ -0,0 +1,32 @@
+import warnings
+from lime.lime_tabular import LimeTabularExplainer
+from lime import submodular_pick
+
+# https://lime-ml.readthedocs.io/en/latest/lime.html
+
+class LIME:
+    def __init__(self, X, predict_fn, num_features=5, features_names=None, result_label='score'):
+
+        self.explainer = LimeTabularExplainer(X, 
+            feature_names=features_names,
+            class_names=[result_label],
+            mode='regression')
+
+        self.predict_fn = predict_fn
+        self.num_features = num_features
+        self.splime = None
+
+    
+    def explain_instance(self, x):
+        return self.explainer.explain_instance(x, self.predict_fn, num_features=self.num_features)
+
+
+    def fit(self, X, sample_size=20, num_expected_examples=15):
+        # https://github.com/marcotcr/lime/blob/master/doc/notebooks/Submodular%20Pick%20examples.ipynb
+        self.splime = submodular_pick.SubmodularPick(self.explainer, X, 
+            self.predict_fn, sample_size=sample_size, 
+            num_features=self.num_features, num_exps_desired=num_expected_examples)
+
+    
+    def get_explanations(self):
+        return self.splime.sp_explanations
